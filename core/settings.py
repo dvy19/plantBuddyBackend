2284e616971c54456c5c9e86c66fc79b4fec7821
dvy19 <divyadixit.311204@gmail.com>
2026-07-26 18:48:01 +0530

    settings updated render url added

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    "localhost,127.0.0.1,"
+    "localhost,127.0.0.1,plantbuddybackend.onrender.com"
 ).split(",")
 
 
